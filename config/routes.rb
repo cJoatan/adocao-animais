6981381db6ecbe83aa61d5992661e5d08a5efe41
cjoatan <cjoatan@gmail.com>
2016-09-10 14:13:24 -0300

    adoptions como url root

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :adoptions_images, only:[:create, :update, :destroy]
+
+  root "adoptions#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
