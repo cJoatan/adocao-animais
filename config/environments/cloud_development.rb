1e652c6f7ba1da716b7a621f49741b690c815639
cjoatan <cjoatan@gmail.com>
2016-09-20 09:35:45 -0300

    email default url cloud_development

--- config/environments/cloud_development.rb
+++ config/environments/cloud_development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   
   # email
-  # config.action_mailer.default_url_options = { :host => 'meupetdev.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'meupetdev.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
