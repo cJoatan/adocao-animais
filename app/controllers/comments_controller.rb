2b78c922dde342811a9d7788428d551f9ad0c449
cjoatan <cjoatan@gmail.com>
2016-09-26 12:51:08 -0300

    remoção de comentários

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,7 +26,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment.adoption, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @comment.adoption }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render @comment.adoption }
@@ -38,9 +38,10 @@ class CommentsController < ApplicationController
   # DELETE /comments/1
   # DELETE /comments/1.json
   def destroy
+    adotion = @comment.adoption
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to adotion }
       format.json { head :no_content }
     end
   end
